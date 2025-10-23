dcd2eeae41bbfbc6ad4b505ea2a8099cac37e800
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-23 19:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function removecolor(){
+    let select=document.getElementById("colorSelect");
+    let selectedIndex=select.selectedIndex;
+    if(selectedIndex!==-1){
+        select.remove(selectedIndex);
+    }
+}
+document.querySelector("input[type='button']").addEventListener("click",removecolor);
